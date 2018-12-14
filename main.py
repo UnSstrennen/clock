722875e8434db902616236ebdb0412cf83be08a7
9p2_15@intra.l1523.ru <9p2_15@intra.l1523.ru>
2018-12-14 14:36:23 +0300

    developed delta counting function

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from time import ctime, localtime, mktime
 from win32api import SetSystemTime
 
 
-
 class ServerTime:
     """ server client class """
     def __init__(self):
@@ -19,8 +18,7 @@ class ServerTime:
     def get_time(self):
         for host in self.hosts:
             try:
-                self.x = self.client.request(host).tx_time
-                self.time_obj = localtime(self.x)
+                self.time_obj = localtime(self.client.request(host).tx_time)
                 if bool(self.time_obj):
                     return {'day': self.time_obj[2], 'month': self.time_obj[1], 'year': self.time_obj[0],
                             'hours': self.time_obj[3], 'minutes': self.time_obj[4], 'seconds': self.time_obj[5],
@@ -40,9 +38,21 @@ def get_local_time():
 
 
 def count_delta():
+    """ counts the time delta """
     server = datetime.fromtimestamp(mktime(ServerTime().time_obj))
     local = datetime.now()
-    print(local)
-    print(server)
     delta = server - local
-    print(delta.seconds)
+    days = delta.days
+    seconds = delta.seconds
+    if seconds >= 60:
+        minutes = seconds // 60
+        seconds -= minutes * 60
+    else:
+        minutes = 0
+    if minutes >= 60:
+        hours = minutes // 60
+        minutes = minutes - hours * 60
+    else:
+        hours = 0
+    microseconds = delta.microseconds
+    return {'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds, 'microseconds': microseconds}
