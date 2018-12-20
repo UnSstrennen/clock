14bc20946078df6a39f31b03f96c422e98ee7d48
KAMIKADZ4 <45710186+KAMIKADZ4@users.noreply.github.com>
2018-12-20 19:27:55 +0300

    Add files via upload

--- Проект.py
+++ Проект.py
@@ -1,20 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QPushButton, QColorDialog, QMainWindow, QComboBox
+from PyQt5.QtWidgets import (QApplication, QPushButton, QColorDialog,
+                             QMainWindow, QComboBox, QLCDNumber, QSlider)
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
 from math import fabs
-import json
 
 
 class Example(QMainWindow):
     def __init__(self):
         super(Example, self).__init__()
         self.setWindowIcon(QtGui.QIcon('icon.png'))
-        with open('cfg.json') as f:
-            self.cfg_dict = json.loads(f.read())
-        self.name_file = self.cfg_dict['background_image']
         self.color_frame = (0, 0, 0)
         self.old_pos = None
+        self.text = ['00', '00']
 
         # Генерация окна
         self.setWindowTitle('Clock')
@@ -47,11 +45,51 @@ class Example(QMainWindow):
         self.btn_st.move(3, 3)
         self.btn_st.show()
 
+        # Кнопка будильника
+        self.alarm = QPushButton(self, clicked=self.alarm_st)
+        self.alarm.setIcon(QtGui.QIcon(QtGui.QPixmap("alarmN.png")))
+        self.alarm.setFlat(True)
+        self.alarm.resize(25, 25)
+        self.alarm.move(204, 39)
+        self.alarm.show()
+
+        # Дисплей для будильника
+        self.lcdA = QLCDNumber(self)
+        self.lcdA.resize(150, 75)
+        self.lcdA.move(55, 30)
+        self.lcdA.setSegmentStyle(QLCDNumber.Flat)
+        self.lcdA.setObjectName("LCDA")
+        self.lcdA.setStyleSheet("#LCDA {background-image: url(фон.png); border: 2px solid #4c4c4c;}")
+        self.lcdA.display(':'.join(self.text))
+
+        # Слайдер выбора часа
+        self.sldH = QSlider(Qt.Vertical, self)
+        self.sldH.setMaximum(23)
+        self.sldH.move(46, 30)
+        self.sldH.resize(9, 75)
+        self.sldH.valueChanged.connect(self.slider)
+
+        # Слайдер выбора минут
+        self.sldM = QSlider(Qt.Vertical, self)
+        self.sldM.setMaximum(59)
+        self.sldM.move(206, 30)
+        self.sldM.resize(9, 75)
+        self.sldM.valueChanged.connect(self.slider2)
+
+        # Дисплей часов
+        self.lcd = QLCDNumber(self)
+        self.lcd.resize(150, 75)
+        self.lcd.setSegmentStyle(QLCDNumber.Flat)
+        self.lcd.move(55, 40)
+        self.lcd.display('07:00')
+        self.lcd.setObjectName("LCD")
+        self.lcd.setStyleSheet("#LCD {background-image: url(фон.png); border: 2px solid #4c4c4c;}")
+
         # Комбобокс для стилизации
         self.combo = QComboBox(self)
         self.combo.setObjectName('Combo')
         self.combo.resize(75, 20)
-        self.combo.addItems(["Stylization", "Color", "OLDS",
+        self.combo.addItems(["Stylization", "Color", "Olds",
                              "Backdrop2", "Backdrop3", "Backdrop4"])
         self.combo.activated[str].connect(self.onActivated)
         self.combo.move(30, 30)
@@ -60,35 +98,52 @@ class Example(QMainWindow):
 
     # Сцена часов
     def general(self):
-        self.background_image()
         try:
             self.btn_st.show()
             self.btn_gn.hide()
             self.combo.hide()
+            self.alarm.show()
+            self.lcd.show()
+            self.sldH.hide()
+            self.sldM.hide()
+            self.lcdA.hide()
         except AttributeError:
             pass
+
 
     # Сцена настроек
     def settings(self):
         self.btn_gn.show()
         self.btn_st.hide()
         self.combo.show()
+        self.alarm.hide()
+        self.lcd.hide()
+        self.sldH.hide()
+        self.sldM.hide()
+        self.lcdA.hide()
+
+    def alarm_st(self):
+        self.btn_gn.show()
+        self.btn_st.hide()
+        self.alarm.hide()
+        self.lcd.hide()
+        self.sldH.show()
+        self.sldM.show()
+        self.lcdA.show()
 
     # Изменение фона окна
     def palette(self):
         color = QColorDialog.getColor()
         if color.isValid():
             self.setStyleSheet("#MainWidget {background-color: %s;}" % color.name())
-            self.paintEvent(self, clr=self.hex_to_rgb(color.name()), palette_call=True)
+            self.paintEvent(self, clr=self.hex_to_rgb(color.name()))
 
     # рисование окна
-    def paintEvent(self, event: QtGui.QPaintEvent, clr=(255, 255, 255), palette_call=False):
-        if palette_call:
-            self.color_frame = clr
+    def paintEvent(self, event: QtGui.QPaintEvent, clr=(0, 0, 0)):
         painter = QtGui.QPainter(self)
-        painter.setPen(QtGui.QPen(QtGui.QColor(int(fabs(self.color_frame[0] - 75)),
-                                               int(fabs(self.color_frame[1] - 75)),
-                                               int(fabs(self.color_frame[2] - 75))), 2))
+        painter.setPen(QtGui.QPen(QtGui.QColor(int(fabs(clr[0] - 75)),
+                                               int(fabs(clr[1] - 75)),
+                                               int(fabs(clr[2] - 75))), 2))
         painter.drawRect(self.rect())
 
     # нужна для перемещёния окна
@@ -109,17 +164,28 @@ class Example(QMainWindow):
         lv = len(value)
         return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
-    # Установка фонового изображения
-    def background_image(self):
-        pass
-
     def onActivated(self, val):
-        backgrounds = {'OLDS': 'olds.jpg', 'Backdrop2': 'Фон.png', 'Backdrop3': '', 'Backdrop4': ''}
+        backgrounds = {'Stylization': '#272727', 'Olds': 'olds.png', 'Backdrop2': 'matrix.png',
+                       'Backdrop3': '', 'Backdrop4': ''}
         if val == 'Color':
             self.palette()
-        elif val not in ('Color', 'Stylization'):
+        elif val == 'Stylization':
+            self.setStyleSheet("#MainWidget {background-color: #272727;}")
+        elif val != 'Color':
             self.setStyleSheet("#MainWidget {background-image: url(%s);}" % backgrounds[val])
             self.paintEvent(self)
+
+    def slider(self, n):
+        if len(str(n)) == 1:
+            n = '0' + str(n)
+        self.text[0] = str(n)
+        self.lcdA.display(':'.join(self.text))
+
+    def slider2(self, n):
+        if len(str(n)) == 1:
+            n = '0' + str(n)
+        self.text[1] = str(n)
+        self.lcdA.display(':'.join(self.text))
 
 
 if __name__ == '__main__':
