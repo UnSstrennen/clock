9afc104ef7f1e3f75bdda5cc69889a6a8f0bdd53
KAMIKADZ4 <you@example.com>
2019-01-04 12:43:48 +0300

    Upgate 3.0

--- Проект.py
+++ Проект.py
@@ -8,6 +8,7 @@ from subprocess import Popen, PIPE, check_call
 from ntplib import NTPClient
 from datetime import datetime, timedelta, timezone
 from time import ctime, localtime
+from winsound import Beep
 
 from functions import get_local_time
 
@@ -103,6 +104,13 @@ class Alarm:
         check_call("TASKKILL /F /PID {pid} /T".format(pid=self.process.pid))
 
 
+def ding_beep(sts):
+    if sts:
+        while True:
+            Beep(800, 100)
+    else:
+        pass
+
 
 class Example(QMainWindow):
     def __init__(self):
@@ -112,26 +120,23 @@ class Example(QMainWindow):
         self.alarm_class = Alarm(0, 0)
         self.status = False
         self.h_m_list = [None, None]
+        self.color_frame = (0, 0, 0)
+        self.old_pos = None
+        self.text = ['00', '00']
         self.initUI()
 
 
     def initUI(self):
-        self.color_frame = (0, 0, 0)
-        self.old_pos = None
-        self.text = ['00', '00']
-
-
-
         # Генерация окна
         self.setWindowTitle('Clock')
-        self.setGeometry(0, 0, 260, 160)
+        self.setGeometry(100, 100, 260, 160)
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setObjectName('MainWidget')
         self.setStyleSheet("#MainWidget {background-color: #272727;}")
 
         # Кнопка закрытие окна
         self.btn_close = QPushButton(self, clicked=self.close)
-        self.btn_close.setIcon(QtGui.QIcon(QtGui.QPixmap('cross.png')))
+        self.btn_close.setIcon(QtGui.QIcon(QtGui.QPixmap('pngs/cross.png')))
         self.btn_close.setFlat(True)
         self.btn_close.resize(17, 17)
         self.btn_close.move(242, 1)
@@ -139,7 +144,7 @@ class Example(QMainWindow):
 
         # Кнопка возрата в general
         self.btn_gn = QPushButton(self, clicked=self.general)
-        self.btn_gn.setIcon(QtGui.QIcon(QtGui.QPixmap('back.png')))
+        self.btn_gn.setIcon(QtGui.QIcon(QtGui.QPixmap('pngs/back.png')))
         self.btn_gn.setFlat(True)
         self.btn_gn.resize(20, 20)
         self.btn_gn.move(1, 1)
@@ -147,7 +152,7 @@ class Example(QMainWindow):
 
         # Кнопка входа в settings
         self.btn_st = QPushButton(self, clicked=self.settings)
-        self.btn_st.setIcon(QtGui.QIcon(QtGui.QPixmap("gear.png")))
+        self.btn_st.setIcon(QtGui.QIcon(QtGui.QPixmap("pngs/gear.png")))
         self.btn_st.setFlat(True)
         self.btn_st.resize(15, 15)
         self.btn_st.move(3, 3)
@@ -155,7 +160,7 @@ class Example(QMainWindow):
 
         # Кнопка будильника
         self.alarmB = QPushButton(self, clicked=self.alarm_st)
-        self.alarmB.setIcon(QtGui.QIcon(QtGui.QPixmap("alarmN.png")))
+        self.alarmB.setIcon(QtGui.QIcon(QtGui.QPixmap("pngs/alarmN.png")))
         self.alarmB.setFlat(True)
         self.alarmB.resize(25, 25)
         self.alarmB.move(204, 39)
@@ -180,7 +185,7 @@ class Example(QMainWindow):
         self.lcdA.setSegmentStyle(QLCDNumber.Flat)
         self.lcdA.setObjectName("LCDA")
         self.lcdA.setStyleSheet(
-            "#LCDA {background-image: url(фон.png); border: 2px solid #4c4c4c;}")
+            "#LCDA {background-image: url(pngs/фон.png); border: 2px solid #4c4c4c;}")
         self.lcdA.display(':'.join(self.text))
 
         # Слайдер выбора часа
@@ -204,11 +209,11 @@ class Example(QMainWindow):
         self.lcd.move(55, 40)
         self.lcd.display(make_time_for_lcd())
         self.lcd.setObjectName("LCD")
-        self.lcd.setStyleSheet("#LCD {background-image: url(фон.png); border: 2px solid #4c4c4c;}")
+        self.lcd.setStyleSheet("#LCD {background-image: url(pngs/фон.png); border: 2px solid #4c4c4c;}")
 
         # Комбобокс для стилизации
         self.combo = QComboBox(self)
-        self.combo.setObjectName('Combo')
+        self.combo.setStyleSheet('border-radius: 3px, 3px, 2px, 1px;')
         self.combo.resize(75, 20)
         self.combo.addItems(["Stylization", "Color", "Olds",
                              "Matrix", "Sofa", "Low Poly", "Anime", "Pony", "Савок"])
@@ -262,8 +267,8 @@ class Example(QMainWindow):
         self.lcd.hide()
         self.sldH.show()
         self.sldM.show()
-        self.sldH.setValue(1)
-        self.sldM.setValue(42)
+        self.sldH.setValue(get_local_time()['hours'])
+        self.sldM.setValue(get_local_time()['minutes'])
         self.lcdA.show()
         self.alVKL.show()
         self.sinT.hide()
@@ -320,9 +325,9 @@ class Example(QMainWindow):
         return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
     def onActivated(self, val):
-        backgrounds = {'Stylization': '#272727', 'Olds': 'olds.png', 'Matrix': 'matrix.png',
-                       'Sofa': 'диван.jpg', 'Low Poly': 'bg.png', "Anime": 'anime.jpg',
-                       "Pony": 'pony.jpg', "Савок": 'фоник.png'}
+        backgrounds = {'Stylization': '#272727', 'Olds': 'pngs/olds.png', 'Matrix': 'pngs/matrix.png',
+                       'Sofa': 'pngs/диван.jpg', 'Low Poly': 'pngs/bg.png', "Anime": 'pngs/anime.jpg',
+                       "Pony": 'pngs/pony.jpg', "Савок": 'pngs/фоник.png'}
         if val == 'Color':
             self.palette()
         elif val == 'Stylization':
@@ -358,12 +363,12 @@ class Example(QMainWindow):
     def alarm_status(self):
         if not self.status:
             self.status = True
-            self.alarmB.setIcon(QtGui.QIcon(QtGui.QPixmap("alarmA.png")))
+            self.alarmB.setIcon(QtGui.QIcon(QtGui.QPixmap("pngs/alarmA.png")))
             self.alVKL.setText('Выключить')
             self.alarm_class.set(self.h_m_list[0], self.h_m_list[1])
         elif self.status:
             self.status = False
-            self.alarmB.setIcon(QtGui.QIcon(QtGui.QPixmap("alarmN.png")))
+            self.alarmB.setIcon(QtGui.QIcon(QtGui.QPixmap("pngs/alarmN.png")))
             self.alVKL.setText('Включить')
         self.alarm_class.tracked = self.status
 
